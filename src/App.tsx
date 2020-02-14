83dc519d9a3148d0529da83f72fea26fbb896ab2
Zora Fuchs <>
2020-02-14 07:13:42 +0100

    add navigation points, make style better

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Card, Container, Image } from 'react-bootstrap';
+import { Card, Container, Image, ListGroup, ListGroupItem, Navbar } from 'react-bootstrap';
 import { NavigationComponent } from './components/NavigationComponent';
 import logo from './logo.svg';
 import img1 from './img1.png';
@@ -7,46 +7,54 @@ import img2 from './img2.png';
 import img3 from './img3.png';
 
 export const App: React.FC = () => (
-  <div>
-    <div className="sticky white-bg">
-      <Image src={logo} fluid />
+  <div className="turq-bg">
+    <div className="sticky">
+      <Image className="logo" src={logo} fluid />
       <NavigationComponent />
     </div>
     <Container>
-      <Card className="mt-5 white-bg">
-        <Card.Title className="white-bg">
-          Samstag 22.3.2020 <br />
-          Doors 20:00
-        </Card.Title>
-        <Card.Img variant="top" src={img1} />
-        <Card.Img variant="top" src={img2} />
+      <Card className="mt-4">
+        <Card.Body>
+          <Card.Title className="date">
+            Samstag 22.3.2020 <br />
+            Doors 20:00
+          </Card.Title>
+          <Card.Img className="mt-3" variant="top" src={img1} />
+          <Card.Img className="mt-3" variant="top" src={img2} />
+        </Card.Body>
       </Card>
     </Container>
 
-    <h1 className="mt-5">Über uns</h1>
+    <Navbar bg="white mt-4">
+      <Navbar.Brand>Über uns</Navbar.Brand>
+    </Navbar>
 
     <Container>
-      <Card className="mt-5 white-bg">
-        <Card.Title className="white-bg">Die Verunstaltung</Card.Title>
-        <Card.Text>
-          tuer adipiscing elit, sed diam no- nummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat vo-
-          lutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
-          ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vul- putate velit esse molestie
-          consequat, vel illum dolore eu feugiat nulla facili- sis at vero eros et accumsan et iusto odio dignissim qui
-          blandit praesent
-        </Card.Text>
-        <Card.Img variant="top" src={img3} />
+      <Card className="mt-4">
+        <Card.Body>
+          <Card.Title>Die Verunstaltung</Card.Title>
+          <Card.Text>
+            tuer adipiscing elit, sed diam no- nummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat vo-
+            lutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+            aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vul- putate velit esse
+            molestie consequat, vel illum dolore eu feugiat nulla facili- sis at vero eros et accumsan et iusto odio
+            dignissim qui blandit praesent
+          </Card.Text>
+          <Card.Img variant="top" src={img3} />
+        </Card.Body>
       </Card>
 
-      <Card className="mt-5 white-bg">
-        <Card.Title className="white-bg">Die Verunstalter</Card.Title>
-        <Card.Text>
-          tuer adipiscing elit, sed diam no- nummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat vo-
-          lutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
-          ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vul- putate velit esse molestie
-          consequat, vel illum dolore eu feugiat nulla facili- sis at vero eros et accumsan et iusto odio dignissim qui
-          blandit praesent
-        </Card.Text>
+      <Card className="mt-4">
+        <Card.Body>
+          <Card.Title>Die Verunstalter</Card.Title>
+          <Card.Text>
+            tuer adipiscing elit, sed diam no- nummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat vo-
+            lutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+            aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vul- putate velit esse
+            molestie consequat, vel illum dolore eu feugiat nulla facili- sis at vero eros et accumsan et iusto odio
+            dignissim qui blandit praesent
+          </Card.Text>
+        </Card.Body>
       </Card>
       <br />
     </Container>
